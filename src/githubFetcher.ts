19b4ce8daabf4ea24ce42da53ea80425066d07cc
kimu_shu <alfvegardrisc@gmail.com>
2017-03-31 21:20:05 +0900

    Add github fetch helper

---
+++ src/githubFetcher.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+import * as pify from 'pify';
+import * as request from 'request';
+import * as GitHub from 'github';
+
+const RAWGITHUB_HOST = "https://raw.githubusercontent.com";
+
+export interface GitHubRepository {
+    owner: string;
+    repo: string;
+    branch?: string;
+}
+
+export function readGithubFile(repo: GitHubRepository, filename: string, encoding: string = null): Promise<Buffer|string> {
+    let url = `${RAWGITHUB_HOST}/${repo.owner}/${repo.repo}/${repo.branch||"master"}/${filename}`;
+    // First, try access from direct rawgithub URL
+    return pify(request)({url, encoding: null}).then((resp) => {
+        return resp.body;
+    }).catch((error) => {
+        // If direct access failed, try again via GitHub API
+        // (This is a more reliable way but consumes API limit)
+        return new GitHub().repos.getContent({
+            owner: repo.owner,
+            repo: repo.repo,
+            ref: repo.branch || "master",
+            path: filename,
+        }).then(({data}) => {
+            return Buffer.from(data.content, data.encoding);
+        });
+    }).then((buf: Buffer) => {
+        if (encoding != null) {
+            return buf.toString(encoding);
+        }
+        return buf;
+    });
+}
