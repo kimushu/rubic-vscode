80338a59541a462ecc7af2f33f17dfbc85413e58
kimu_shu <alfvegardrisc@gmail.com>
2017-03-31 21:14:13 +0900

    Add piccolo board support

---
+++ src/peridotPiccoloBoard.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+import { PeridotClassicBoard } from './peridotClassicBoard';
+
+export class PeridotPiccoloBoard extends PeridotClassicBoard {
+    constructor(path: string) {
+        super(path);
+    }
+}
