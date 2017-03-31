82578992ec78fe66e5aa046bb493c00d97369d27
kimu_shu <alfvegardrisc@gmail.com>
2017-03-31 21:20:48 +0900

    Add version getter

---
+++ src/rubicVersion.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+import * as path from 'path';
+
+/**
+ * Stores version string of Rubic itself
+ */
+export const RUBIC_VERSION: string = (() => {
+    return require(path.join(__dirname, "..", "..", "package.json")).version;
+})();
