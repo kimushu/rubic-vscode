61c03d9fea165323d2ec46bd95af33ab69cee468
kimu_shu <alfvegardrisc@gmail.com>
2017-04-15 14:24:53 +0900

    Fix filter for Piccolo

--- src/peridotPiccoloBoard.ts
+++ src/peridotPiccoloBoard.ts
@@ -1,9 +1,15 @@
 'use strict';
 
 import { PeridotClassicBoard } from './peridotClassicBoard';
+import { BoardCandidate } from './rubicBoard';
 
 export class PeridotPiccoloBoard extends PeridotClassicBoard {
     constructor(path: string) {
         super(path);
+    }
+
+    protected static judgeSupportedOrNot(candidate: BoardCandidate): void {
+        // Nothing to do
+        // Piccolo has no fixed USB-UART device, so all VCP devices may be used as piccolo
     }
 }
