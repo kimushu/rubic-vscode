e52a9737eca7cec89e1c0d55c134391e3772cd5d
kimu_shu <alfvegardrisc@gmail.com>
2017-11-13 13:06:16 +0900

    Debug travis

--- tools/run-test.js
+++ tools/run-test.js
@@ -15,12 +15,13 @@ tests.forEach((test) => {
     console.log("#".repeat(100));
     console.log(`# [${test}] Started at ${new Date().toString()}`);
     console.log("");
+    let env = Object.assign({}, process.env, {
+        CODE_TESTS_PATH: testRoot,
+        CODE_TESTS_WORKSPACE: workspace
+    });
+    delete env["ELECTRON_RUN_AS_NODE"];
     let result = cp.spawnSync("node", [tester], {
-        env: Object.assign({}, process.env, {
-            ELECTRON_RUN_AS_NODE: 0,
-            CODE_TESTS_PATH: testRoot,
-            CODE_TESTS_WORKSPACE: workspace
-        }),
+        env,
         stdio: "inherit"
     });
     console.log(`# [${test}] Finished at ${new Date().toString()} (result=${result.status})`);
